a9cfbc3306ffd88cc841d02dcc3ffd805dd44fbf
SheepTester <ussa.say@gmail.com>
2020-05-03 22:41:00 -0700

    Copy a lot of the intro code lol; in the future we'll have to reduce repetition oof

--- cocnept-designs/sean/injection.js
+++ cocnept-designs/sean/injection.js
@@ -1,9 +1,21 @@
 (() => {
   const params = new URL(location).searchParams
   if (params.get('principal-demo')) {
-    document.head.appendChild(document.createElement('script')).src = '../sean/principal-demo.js'
+    const script = document.head.appendChild(document.createElement('script'))
+    script.src = '../sean/principal-demo.js'
     const link = document.head.appendChild(document.createElement('link'))
     link.href = '../sean/principal-demo.css'
     link.rel = 'stylesheet'
+
+    script.addEventListener('load', e => {
+      fetch('../sean/principal-demo.json')
+        .then(r => r.json())
+        .then(async json => {
+          const demo = new PrincipalDemo()
+            .addTo(document.body)
+          await demo.start(json)
+          demo.removeFromParent()
+        })
+    })
   }
 })()
--- cocnept-designs/sean/principal-demo.js
+++ cocnept-designs/sean/principal-demo.js
@@ -1,1 +1,120 @@
-console.log('hi')
+function pause (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+function bindMethods (instance, methods) {
+  for (const methodName of methods) {
+    instance[methodName] = instance[methodName].bind(instance)
+  }
+}
+
+class PrincipalDemo {
+  constructor () {
+    bindMethods(this, [
+      '_onKeyDown'
+    ])
+
+    this._initElems()
+  }
+
+  _initElems () {
+    const thumbnail = document.createElement('img')
+    thumbnail.className = 'demo-thumbnail'
+
+    const spoken = document.createElement('span')
+    spoken.className = 'demo-spoken'
+
+    const toSpeak = document.createElement('span')
+    toSpeak.className = 'demo-to-speak'
+
+    const dialogue = document.createElement('p')
+    dialogue.className = 'demo-dialogue'
+    dialogue.appendChild(spoken)
+    dialogue.appendChild(toSpeak)
+
+    const ok = document.createElement('button')
+    ok.className = 'demo-ok'
+    ok.textContent = 'OK'
+
+    const options = document.createElement('div')
+    options.className = 'demo-ok-wrapper'
+    options.appendChild(ok)
+
+    const wrapper = document.createElement('div')
+    wrapper.className = 'demo-wrapper'
+    wrapper.appendChild(thumbnail)
+    wrapper.appendChild(dialogue)
+    wrapper.appendChild(options)
+
+    this.elems = {
+      thumbnail,
+      spoken,
+      toSpeak,
+      dialogue,
+      ok,
+      wrapper
+    }
+  }
+
+  setThumbnail (url, colour) {
+    this.elems.thumbnail.src = url
+    if (colour !== undefined) {
+      this.elems.thumbnail.style.backgroundColor = colour
+    }
+    return this
+  }
+
+  addTo (parent) {
+    parent.appendChild(this.elems.wrapper)
+    return this
+  }
+
+  removeFromParent () {
+    this.elems.wrapper.remove()
+    return this
+  }
+
+  async _animateSpeak (text, delay = 20) {
+    const { spoken, toSpeak } = this.elems
+    for (let i = 0; i < text.length; i++) {
+      if (this.skipDialog) break
+
+      spoken.textContent = text.slice(0, i)
+      toSpeak.textContent = text.slice(i)
+      await pause(delay)
+    }
+    spoken.textContent = text
+    toSpeak.textContent = ''
+  }
+
+  _onKeyDown (e) {
+    if (e.key === 'Enter') {
+      this.skipDialog = true
+    }
+  }
+
+  async start (dialogueData) {
+    const {
+      dialogue: dialogueText,
+      ok
+    } = this.elems
+
+    document.addEventListener('keydown', this._onKeyDown)
+
+    for (const { say, select } of dialogueData) {
+      ok.classList.add('demo-hide-ok')
+
+      this.skipDialog = false
+      await this._animateSpeak(say)
+
+      ok.classList.remove('demo-hide-ok')
+      ok.focus()
+      // TODO: await ok click
+      await pause(1000)
+    }
+
+    document.removeEventListener('keydown', this._onKeyDown)
+
+    return data
+  }
+}
